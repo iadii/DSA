7ea5295a8e224cde60ece9763e988a0837673d2b
Aditya Mishra <mishraaditya3456@gmail.com>
2025-05-05 12:46:52 +0530

    basic

---
+++ 01_basic cpp/sumTwoint.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+int main () {
+   int a=5 , b=6;
+
+   int sum = a+b;
+   cout<<sum;
+   return 0;
+}
---
+++ 01_basic cpp/swap.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+int main () {
+   int a=5, b=6, temp;
+
+   temp=a;
+   a=b;
+   b=temp;
+
+   cout<<a<<b<<temp;
+   return 0;
+}

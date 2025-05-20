fce3a083576135cba4e3f93cb2b65cf7e7cc82c0
Aditya Mishra <mishraaditya3456@gmail.com>
2025-05-20 17:09:31 +0530

    bitwise file

---
+++ 04_Btwise/uniqueElement.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+int main () {
+   int arr[] = {2,3,4,5,4,3,2};
+
+   int result = 0;
+   for(int i = 0; i<7; i++){
+        result ^= arr[i];
+    
+   }
+   
+   cout<<result;
+   return 0;
+}

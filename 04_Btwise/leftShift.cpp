5f2f99e7d56a1adeeacb7f7d6db1941d98d12b8a
Aditya Mishra <mishraaditya3456@gmail.com>
2025-05-29 07:25:45 +0530

    added remarks

--- 04_Btwise/leftShift.cpp
+++ 04_Btwise/leftShift.cpp
@@ -5,19 +5,19 @@ int main () {
 
     // when we left shift by 2 we multiply that number 2^2
     // if num is 2 then left shift 1 will be 2 * 2^2 = 8
-    // again we lwft shift by 1 , 8 * 2^2 = 16
+    // again we left shift by 1 , 8 * 2^2 = 16
     int leftShiftby2 = a<<2;
     cout<<leftShiftby2<<endl;
 
     // when we left shift by 1 we multiply that number 2^1 
     // if num is 2 then left shift 1 will be 2 * 2^1 = 4
-    // again we lwft shift by 1 , 4 * 2^1 = 8
+    // again we left shift by 1 , 4 * 2^1 = 8
     int leftShift1 = a<<1;
     cout<<leftShift1<<endl;
     
     // when we left shift by 3 we multiply that number 2^3
     // if num is 2 then left shift 1 will be 2 * 2^3 = 16
-    // again we lwft shift by 1 , 16 * 2^3 = 128
+    // again we left shift by 1 , 16 * 2^3 = 128
     int leftShift3 = a<<3;
     cout<<leftShift3;
    return 0;
--- 04_Btwise/rightShift.cpp
+++ 04_Btwise/rightShift.cpp
@@ -1,14 +1,22 @@
 #include<bits/stdc++.h>
 int main () {
    
-    int a = 5;
+    int a = 100;
 
+    // if we right shift any num by n time the it will num / 2^n
+
+    // when we right shift by 2 it will divided by number/ 2^2
+    // if num is 100 then right shift 1 will be 100 / 2^2 = 25
     int rightShiftby2 = a>>2;
     cout<<rightShiftby2<<endl;
 
+    // when we right shift by 2 it will divided by number/ 2^1
+    // if num is 100 then right shift 1 will be 100 / 2^1 = 50
     int rightShift1 = a>>1;
     cout<<rightShift1<<endl;
 
+    // when we right shift by 2 it will divided by number/ 2^3
+    // if num is 100 then right shift 1 will be 100 / 2^3 = 12
     int rightShift3 = a>>3;
     cout<<rightShift3;
    return 0;

6cd404038ac1be85ae013347c1279f1c2c0d4e14
Aditya Mishra <mishraaditya3456@gmail.com>
2025-06-19 05:11:58 +0530

    extreme Print

--- 02_Array/extremePrint.cpp
+++ 02_Array/extremePrint.cpp
@@ -7,21 +7,21 @@ int main()
 
     int left = 0;
     int right = size - 1;
-    while (left <= right){
+    while (left <= right)
+    {
 
-        if (left == right) {
-        cout << arr[left] << " ";
-    }
-    else{
-        cout<<arr[left]<<" ";
-        cout<<arr[right]<<" ";
-
-    }
+        if (left == right)
+        {
+            cout << arr[left] << " ";
+        }
+        else
+        {
+            cout << arr[left] << " ";
+            cout << arr[right] << " ";
+        }
         left++;
         right--;
-
-       
     }
 
-        return 0;
+    return 0;
 }

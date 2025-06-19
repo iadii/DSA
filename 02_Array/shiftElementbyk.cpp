a18baf4da5c259cd0fd19c49e390a2abde46ac10
Aditya Mishra <mishraaditya3456@gmail.com>
2025-06-20 05:09:48 +0530

    right rotation by kth time

--- 02_Array/shiftElementbyk.cpp
+++ 02_Array/shiftElementbyk.cpp
@@ -4,8 +4,7 @@ int main () {
     int arr[] = {10, 20, 30, 40, 50, 60};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    int k = 2;
-    k = k % size;
+    int k = 3;
     int temp[k];
     for(int i = 0; i < k; i++) {
         temp[i] = arr[size - k + i];

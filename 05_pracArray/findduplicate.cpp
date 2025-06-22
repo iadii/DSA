4ef745af3f521aee02ad4d61fb37d544f81ad00a
Aditya Mishra <mishraaditya3456@gmail.com>
2025-06-23 04:20:44 +0530

    problems

---
+++ 05_pracArray/findduplicate.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+int findDuplicate(int arr[], int size){
+    
+
+}
+
+int main () {
+
+    int arr[] = {1, 3, 4, 2, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+   int index = findDuplicate(arr, size);
+   return 0;
+}

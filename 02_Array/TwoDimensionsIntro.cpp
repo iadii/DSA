63ed03e2b0d0fdcc376b70a19f40c38cbf275b72
Aditya Mishra <mishraaditya3456@gmail.com>
2025-06-30 06:29:21 +0530

    problems

--- 02_Array/TwoDimensionsIntro.cpp
+++ 02_Array/TwoDimensionsIntro.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main() {
     // Declare and initialize a 2D array with 3 rows and 4 columns
+    // atleast i have to provide column size
+    // int arr[][5]
+    // In memory, the column size is needed to calculate the linear index.
+    // Formula: (total columns) * row_index + column_index
+    // For finding 8 (which is at arr[1][3]): 4 * 1 + 3 = 7 (by bodmas)
     int arr[3][4] = {
         {1, 2, 3, 4},
         {5, 6, 7, 8},

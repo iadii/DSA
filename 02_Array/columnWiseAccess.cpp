1b037aaf5c0af4cbaa7d8d0a56ecee9b20596b74
Aditya Mishra <mishraaditya3456@gmail.com>
2025-07-09 23:21:00 +0530

    problems

--- 02_Array/columnWiseAccess.cpp
+++ 02_Array/columnWiseAccess.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 int main () {
 
-   int arr[3][4] = {
+   int arr[4][4] = {
         {1, 2, 3, 4},
         {5, 6, 7, 8},
         {9, 10, 11, 12},
-        {13, 14, 15, 16},
+        {13, 14, 15, 16}
     };
     int size = sizeof(arr) / sizeof(arr[0]);
     int size1 = sizeof(arr[0]) / sizeof(arr[0][0]);

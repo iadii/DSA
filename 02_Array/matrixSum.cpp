a689ee5d37e930d695ebb413d30bea43877ff2ff
Aditya Mishra <mishraaditya3456@gmail.com>
2025-07-14 22:16:14 +0530

    matrixSum

---
+++ 02_Array/matrixSum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Create a 3x3 matrix and initialize with values
+    int matrix[3][3] = {
+        {2, 4, 6},
+        {8, 10, 12},
+        {14, 16, 18}
+    };
+
+    // Calculate the sum of all elements
+    int sum = 0;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            sum += matrix[i][j];
+        }
+    }
+
+    cout << "Sum of all elements in the matrix: " << sum << endl;
+
+    return 0;
+}

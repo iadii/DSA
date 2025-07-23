49888b5f6d1d51e397bb50d9f64e04b532ea3bfa
Aditya Mishra <mishraaditya3456@gmail.com>
2025-07-23 23:24:16 +0530

    row max sum

---
+++ 02_Array/matrix/rowMaxSum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cin >> rows >> cols;
+    vector<vector<int>> matrix(rows, vector<int>(cols));
+    for (int i = 0; i < rows; ++i)
+        for (int j = 0; j < cols; ++j)
+            cin >> matrix[i][j];
+
+    int maxSum = INT_MIN, maxRow = 0;
+    for (int i = 0; i < rows; ++i) {
+        int sum = 0;
+        for (int j = 0; j < cols; ++j)
+            sum += matrix[i][j];
+        if (sum > maxSum) {
+            maxSum = sum;
+            maxRow = i;
+        }
+    }
+    cout << maxRow << endl;
+    return 0;
+}

4094998935646ae9573b6c5a9dd7526d683f829b
Aditya Mishra <mishraaditya3456@gmail.com>
2025-07-28 03:00:50 +0530

    vector

---
+++ 02_Array/vector/dynamicArray.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int dynamic_Arr(int arr[],int num){
+    for(int i=0;i<num;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<num;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
+
+int main () {
+   int num;
+   cin>>num;
+   int* arr = new int[num];
+   dynamic_Arr(arr, num);
+   return 0;
+}

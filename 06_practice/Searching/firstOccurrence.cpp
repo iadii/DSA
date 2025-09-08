2a6b3fae6b9d883543fe24b7edc0792f4c55002d
Aditya Mishra <mishraaditya3456@gmail.com>
2025-09-09 04:38:51 +0530

    practising searching

---
+++ 06_practice/Searching/firstOccurrence.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+int firstOccurence(vector<int> arr, int target){
+    int start = 0;
+    int end = arr.size() -1;
+    int occurrence = -1;
+    while(start <= end){
+        int mid = start + (end - start)/2;
+
+        if(target == arr[mid]){
+            occurrence = mid;
+            end = mid - 1;
+        }
+        else if(target < arr[mid]){
+            end = mid - 1;
+        }
+        else if(target > arr[mid]){
+            start = mid + 1;
+        }
+    }
+    return occurrence;
+}
+
+int main () {
+   
+    vector<int> arr = {
+        10, 20, 30, 30, 30, 30, 30, 30, 40, 50
+    };
+    int target = 30;
+
+    int index = firstOccurence(arr, target);
+    cout<<"first occurrence of "<<target<<" is at index : "<<index;
+   return 0;
+}

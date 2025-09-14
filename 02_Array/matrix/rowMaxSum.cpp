1647f0fcfc57b67646d9dc6d03a5f3b1002bdf06
Aditya Mishra <mishraaditya3456@gmail.com>
2025-09-14 21:07:42 +0530

    updated code

--- 02_Array/matrix/rowMaxSum.cpp
+++ 02_Array/matrix/rowMaxSum.cpp
@@ -1,12 +1,17 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
 int main() {
-    int rows, cols;
-    cin >> rows >> cols;
-    vector<vector<int>> matrix(rows, vector<int>(cols));
-    for (int i = 0; i < rows; ++i)
-        for (int j = 0; j < cols; ++j)
-            cin >> matrix[i][j];
+    vector<vector<int>> matrix = {
+        {2, 4, 6},
+        {8, 10, 102},
+        {14, 16, 18}
+    };
+    
+    // Get actual dimensions from matrix
+    int rows = matrix.size();
+    int cols = matrix[0].size();
 
     int maxSum = INT_MIN, maxRow = 0;
     for (int i = 0; i < rows; ++i) {
@@ -18,6 +23,8 @@ int main() {
             maxRow = i;
         }
     }
-    cout << maxRow << endl;
+    cout << "Row with maximum sum: " << maxRow << endl;
+    cout << "Maximum sum: " << maxSum << endl;
+    
     return 0;
 }

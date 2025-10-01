1d448662072b09e12fd015bcf55d3af34134925f
Aditya Mishra <mishraaditya3456@gmail.com>
2025-10-01 17:21:35 +0530

    practising

---
+++ 06_practice/Searching/lastOccurrence.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+int firstOccur(vector<int> arr, int target){
+    int start = 0;
+    int end = arr.size() -1;
+
+    int ans = -1;
+    while(start <= end){
+        int mid = start + (end - start) /2;
+
+        if(arr[mid] == target){
+            ans = mid;
+            start = mid + 1;
+        }
+        else if (arr[mid] > target){
+            end = mid -1;
+        }
+        else if (arr[mid] < target){
+            start = mid +1;
+        }
+        
+    }
+    return ans;
+}
+int main () {
+   
+    vector<int> arr = {
+        1, 2, 3, 3, 3, 3, 7, 8, 9
+    };
+    int target = 3;
+    int index = firstOccur(arr, target);
+    cout<<"last occurrence of item is at index: "<<index;
+   return 0;
+}

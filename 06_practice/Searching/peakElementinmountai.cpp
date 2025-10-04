a780df814f87b9fed9e761f3d981a832eb5e66a4
Aditya Mishra <mishraaditya3456@gmail.com>
2025-10-04 23:20:39 +0530

    practising

--- 06_practice/Searching/peakElementinmountai.cpp
+++ 06_practice/Searching/peakElementinmountai.cpp
@@ -20,8 +20,8 @@ int peakElement(vector<int> arr){
 }
 int main () {
    
-    vector<int> arr = {0,10,5,2};
-   //  vector<int> arr = {1, 2, 1, 3, 5, 6, 4};
+   //  vector<int> arr = {0,10,5,2};
+    vector<int> arr = {1, 2, 1, 3, 5, 6, 4};
 
     int value = peakElement(arr);
     cout<<"peak Element is "<<arr[value];

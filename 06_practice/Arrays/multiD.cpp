bd7428d8cdc798f5e80f94f17de54d3953215df2
Aditya Mishra <mishraaditya3456@gmail.com>
2025-11-09 21:25:21 +0530

    prac

--- 06_practice/Arrays/multiD.cpp
+++ 06_practice/Arrays/multiD.cpp
@@ -1,6 +1,17 @@
 #include<bits/stdc++.h>
 int multiD(vector <vector<int>> arr){
 
+    for (int i = 0; i < 3; i++)
+    {
+        cout<<"{ ";
+        for (int j = 0; j < 3; j++)
+        {
+            cout<<arr[j][i]<<",";
+        }
+        cout<<" }";
+        cout<<endl;
+    }
+    
 }
 int main () {
    
@@ -9,5 +20,7 @@ int main () {
         {4,5,6},
         {7,8,9}
     };
+
+    multiD(arr);
    return 0;
 }

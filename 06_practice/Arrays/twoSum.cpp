a9347216649df731c80a00feb762c258a4585352
Aditya Mishra <mishraaditya3456@gmail.com>
2025-11-25 21:32:44 +0530

    practising

---
+++ 06_practice/Arrays/twoSum.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+vector<int> twoSum(vector<int> arr, int target){
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr.size(); j++)
+        {
+            if(arr[i] + arr[j] == target){
+                return {i, j};
+            }
+        }
+        
+    }
+    
+    return {};
+}
+int main () {
+    vector<int> arr = {2,7,11,15};
+    int target = 26;
+    vector<int> result = twoSum(arr, target);
+
+    cout << "Indices: " << result[0] << " " << result[1] << endl;
+    cout << "Values: " << arr[result[0]] << " " << arr[result[1]] << endl;
+
+   return 0;
+}

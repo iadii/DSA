b046e210231a9b9d6a5812bcc07f6cec01460a89
Aditya Mishra <mishraaditya3456@gmail.com>
2025-11-25 21:40:39 +0530

    practising

--- 06_practice/Arrays/twoSum.cpp
+++ 06_practice/Arrays/twoSum.cpp
@@ -3,7 +3,7 @@ vector<int> twoSum(vector<int> arr, int target){
 
     for (int i = 0; i < arr.size(); i++)
     {
-        for (int j = 0; j < arr.size(); j++)
+        for (int j = i + 1; j < arr.size(); j++)
         {
             if(arr[i] + arr[j] == target){
                 return {i, j};

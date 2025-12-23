8c60a4dba094cd118c988b002e5be6e35b9f8af5
Aditya Mishra <mishraaditya3456@gmail.com>
2025-12-23 20:35:08 +0530

    practising

--- 06_practice/Arrays/rotateElementby1.cpp
+++ 06_practice/Arrays/rotateElementby1.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-vector <int> rotate(vector <int> arr){
+vector <int> rightRotate(vector <int> arr){
 
     int temp = arr[0];
     for (int i = 0; i < arr.size(); i++){
@@ -13,9 +13,27 @@ vector <int> rotate(vector <int> arr){
     
     return {0};
 }
+vector <int> leftRotate(vector <int> arr){
+
+    int temp = arr[arr.size() - 1];
+    for (int i = arr.size() - 1; i > 0; i--){
+        arr[i] = arr[i - 1];
+    }
+    arr[0] = temp;
+
+    for(int val : arr){
+        cout<<val<<" ";
+    }
+    
+    return {0};
+}
+
+
 int main () {
    
     vector<int> arr = {2, 5, 8, 11, 12, 14, 17};
-    rotate(arr);
+    rightRotate(arr);
+    cout<<endl;
+    leftRotate(arr);
    return 0;
 }

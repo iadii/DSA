0b9ba7215132c46a800e77cc479c791440166ff6
Aditya Mishra <mishraaditya3456@gmail.com>
2025-12-28 11:04:35 +0530

    incomplete dnf will solve it later

--- 06_practice/Arrays/sortColors.cpp
+++ 06_practice/Arrays/sortColors.cpp
@@ -2,7 +2,7 @@
 vector<int> sortColors (vector<int> arr){
     int start = 0, mid = 0;
     int end = arr.size() -1 ;
-
+  //will solve it later
 }
 int main () {
    vector<int> arr = {2, 0, 2, 1, 1, 0, 1, 0, 1, 0};

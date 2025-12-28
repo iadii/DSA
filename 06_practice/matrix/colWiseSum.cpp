b1ad55d330794a96a9c95a416c678f5d7a6004ac
Aditya Mishra <mishraaditya3456@gmail.com>
2025-12-28 16:08:07 +0530

    matrix practise

--- 06_practice/matrix/colWiseSum.cpp
+++ 06_practice/matrix/colWiseSum.cpp
@@ -1,22 +1,18 @@
 #include<bits/stdc++.h>
-int sum( vector<vector<int>> arr){
-    
+vector <int> sum(vector<vector<int>> arr){
     int row = arr.size();           
-    int col = arr[0].size();
+    int col = arr[0].size();       
     for(int i = 0; i < row; i++){
         int sum = 0;
-        for (int j = 0; j < col; j++)
-        {
-            sum += arr[i][j];
+        for(int j = 0; j < col; j++){
+            sum += arr[j][i];
         }
         cout<<sum<<" ";
-        
     }
-    return 0;
+    return {};
 }
 int main () {
-   
-    vector<vector<int>> arr = {
+   vector<vector<int>> arr = {
         {1,2,3},
         {4,5,6},
         {7,8,9}

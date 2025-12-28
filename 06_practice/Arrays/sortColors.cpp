fa046d04b4fafe70fd067bd535ae3c42264d490e
Aditya Mishra <mishraaditya3456@gmail.com>
2025-12-28 11:03:34 +0530

    incomplete dnf

---
+++ 06_practice/Arrays/sortColors.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+vector<int> sortColors (vector<int> arr){
+    int start = 0, mid = 0;
+    int end = arr.size() -1 ;
+
+}
+int main () {
+   vector<int> arr = {2, 0, 2, 1, 1, 0, 1, 0, 1, 0};
+
+//    we will sort colors using dutch national flag algorithm
+   sortColors(arr);
+   return 0;
+}   

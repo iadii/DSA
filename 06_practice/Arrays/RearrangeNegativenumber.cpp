445f44f278b2a65a39545e414cec6ae944240f82
iadii <mishraaditya3456@gmail.com>
2026-01-04 20:55:05 +0530

    practising

---
+++ 06_practice/Arrays/RearrangeNegativenumber.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+vector<int> ReArrange(vector<int> arr){
+    vector<int> neg, vector<int> pos;
+
+}
+int main () {
+   
+    vector<int> arr = {
+        3,1,-2,-5,2,-4
+    };
+
+     
+   return 0;
+}

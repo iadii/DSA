e516991a87b946204cc37ab298016df37bc3518c
iadii <mishraaditya3456@gmail.com>
2026-01-04 20:56:24 +0530

    practising

--- 06_practice/Arrays/RearrangeNegativenumber.cpp
+++ 06_practice/Arrays/RearrangeNegativenumber.cpp
@@ -2,7 +2,9 @@
 vector<int> ReArrange(vector<int> arr){
     vector<int> neg, vector<int> pos;
     for(int i = 0; i < arr.size(); i++){
-        if()
+        if(arr[i] < 0){
+            neg.pushback(arr[i]);
+        }
     }
 }
 int main () {

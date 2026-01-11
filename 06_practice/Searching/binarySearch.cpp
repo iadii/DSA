4436061babe8bfaefa985094ce78801dd9545ffb
iadii <mishraaditya3456@gmail.com>
2026-01-11 10:23:20 +0530

    b search

--- 06_practice/Searching/binarySearch.cpp
+++ 06_practice/Searching/binarySearch.cpp
@@ -4,7 +4,7 @@ vector<int> binarySearch(vector<int> arr, int target){
     int start = 0;
     int end = arr.size() - 1;
     while(start <= end){
-        int mid = (start + (end - start)) /2;
+        int mid = start + (end - start) /2;
 
         if(arr[mid] == target){
             return { mid };

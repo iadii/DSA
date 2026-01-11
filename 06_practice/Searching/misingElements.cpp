7874487a40da7aa0b334cb7a84230d6e6cefc1ad
iadii <mishraaditya3456@gmail.com>
2026-01-11 12:30:05 +0530

    solving leetcode

---
+++ 06_practice/Searching/misingElements.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+vector<int> missigElement(vector<int> arr, target)
+int main () {
+     vector<int> arr = {1,2,3,4,6,7,8};
+
+    int index = missingElement(arr);
+    int missingValue = index + 1;  // Because arr[index] - index > 1, so missing is index + 1
+
+    cout << "Missing element should be at index " << index << " and element is " << missingValue;
+   
+   return 0;
+}

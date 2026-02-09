9fea489e96369fd1b26c57cea9fd75f471e50c71
iadii <mishraaditya3456@gmail.com>
2026-02-09 20:48:52 +0530

    practising first occurrence

--- 06_practice/Searching/firstOccuence.cpp
+++ 06_practice/Searching/firstOccuence.cpp
@@ -25,7 +25,7 @@ int main () {
         10, 20, 30, 30, 30, 40, 50
     };
     int target = 30;
-    int val = firstOccurrence(arr, target);
+    vector<int> val = firstOccurrence(arr, target);
     cout<<"first Occurrence is at: "<<val[0]<<" and value is: "<<arr[val[0]];
    return 0;
 }

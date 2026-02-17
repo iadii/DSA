23d1ff9dc3cf6cf822b60ed0ddd3af0f6cf1d860
iadii <mishraaditya3456@gmail.com>
2026-02-17 19:15:12 +0530

    peak elemnent using binary search

--- 06_practice/Searching/peakElement.cpp
+++ 06_practice/Searching/peakElement.cpp
@@ -1,12 +1,24 @@
 #include<bits/stdc++.h>
 vector<int> peakELement(vector<int> arr){
     int n = arr.size();
-    
-    return {-1};
+    int s = 0;
+    int e = arr.size() - 1;
+    int ans = -1;
+    while(s <= e){
+        int mid = s + (e - s) / 2;
+        if(arr[mid] < arr[mid + 1]){
+            s = mid + 1;
+        }
+        else if(arr[mid] > arr[mid + 1]){
+            ans = mid;
+            e = mid - 1;
+        }
+     }
+    return { ans };
 }
 int main () {
 
-   vector<int> arr = {10, 20, 50, 30, 40};
+   vector<int> arr = {10, 20, 30, 60, 50, 40};
    vector<int> val = peakELement(arr);
 
    cout<<"Peak element is at index: "<<val[0]<<" element is: "<<arr[val[0]];

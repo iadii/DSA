a8aa3e2805e62d8b67763be9399d94c4fe394beb
iadii <mishraaditya3456@gmail.com>
2026-02-21 05:20:32 +0530

    Solved Sort colors based on DNf

--- 06_practice/Arrays/sortColors.cpp
+++ 06_practice/Arrays/sortColors.cpp
@@ -1,8 +1,26 @@
 #include<bits/stdc++.h>
-vector<int> sortColors (vector<int> arr){
+int sortColors (vector<int> arr){
     int start = 0, mid = 0;
     int end = arr.size() -1 ;
-  //will solve it later
+   while(mid <= end){
+      if(arr[mid] == 0){
+         swap(arr[start], arr[mid]);
+         start++;
+         mid++;
+      }
+      else if(arr[mid] == 1){
+         mid++;
+      }
+      else if(arr[mid] == 2){
+         swap(arr[end], arr[mid]);
+         end--;
+      } 
+   }
+
+   for(int nums: arr){
+      cout<<nums<<" ";
+   }
+   return 0;
 }
 int main () {
    vector<int> arr = {2, 0, 2, 1, 1, 0, 1, 0, 1, 0};

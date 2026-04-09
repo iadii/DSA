e0bc109c0767f339313264a00ed8c5b85aec98e4
iadii <mishraaditya3456@gmail.com>
2026-04-09 16:26:19 +0530

    prac

--- 06_practice/Arrays/linearSeach.cpp
+++ 06_practice/Arrays/linearSeach.cpp
@@ -1,22 +1,16 @@
 #include<bits/stdc++.h>
-vector<int> search(vector<int> arr, int item){
-
-    for (int i = 0; i < arr.size(); i++){
-
-        if(arr[i] == item){
-            return {i};
+vector<int> linearSearch(vector<int> arr, int target){
+    for(int i = 0; i < arr.size(); i++){
+        if(arr[i] == target){
+            return {i, target};
         }
     }
-    
-    return {-1};
+    return {-1, target};
 }
 int main () {
-   
-    vector<int> arr = {2, 5, 8, 11, 12, 14, 17};
-    int item = 12;
-
-    vector<int> value = search(arr, item);
-
-    cout<<item<<" found at index "<<value[0];
+   vector<int> arr = {42, 7, 89, 13, 65, 21, 34, 90, 2, 77, 56, 18, 99, 45, 61, 73, 28, 44, 10, 81};
+   int target = 45;
+   vector<int> val = linearSearch(arr, target);
+    cout<<val[1]<<" element is found at index "<<val[0];
    return 0;
 }

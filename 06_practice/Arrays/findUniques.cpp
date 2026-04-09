5337287838836800a162460605da2c0a6e6691cf
iadii <mishraaditya3456@gmail.com>
2026-04-09 16:50:08 +0530

    prac

--- 06_practice/Arrays/findUniques.cpp
+++ 06_practice/Arrays/findUniques.cpp
@@ -5,13 +5,10 @@ vector<int> findUnique(vector<int> arr){
     for(int i = 0; i < arr.size(); i++){
         unique ^= arr[i];
     }
-
     return {unique};
-
 }
 int main () {
    vector<int> arr = { 3, 5, 3, 8, 5, 9, 8 };
-//    vector<int> arr = { 0,0,1,4,2,2,3,3,4 };
    vector<int> val = findUnique(arr);
    cout<<"unique element is : "<<val[0]<<" ";
    return 0;

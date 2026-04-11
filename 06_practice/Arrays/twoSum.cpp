a0afccd5361b89afd211a74055eb1c233af63463
iadii <mishraaditya3456@gmail.com>
2026-04-11 12:02:42 +0530

    updated

--- 06_practice/Arrays/twoSum.cpp
+++ 06_practice/Arrays/twoSum.cpp
@@ -1,21 +1,18 @@
 #include <bits/stdc++.h>
-vector<int> twoSum(vector<int> nums, int target){
-
-    int n = nums.size();
-    for (int i = 0; i < n; i++){
-        for (int j = i + 1; j < n; j++){
-            if (nums[i] + nums[j] == target){
+vector<int> twoSum(vector<int> arr, int target){
+    for(int i = 0; i < arr.size(); i++){
+        for(int j = 1; j < arr.size(); j++){
+            if(arr[i] + arr[j] == target){
                 return {i, j};
             }
         }
     }
-     
     return {-1};
 }
 int main(){
 
     vector<int> arr = {2, 7, 11, 15};
-    int target = 26;
+    int target = 18;
     vector<int> val = twoSum(arr, target);
     
     for(int val : val){

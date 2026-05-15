39911cf54f8406b0edab374212612414e353d88f
iadii <mishraaditya3456@gmail.com>
2026-05-15 11:41:51 +0530

    prac

--- 06_practice/Searching/binarySearch.cpp
+++ 06_practice/Searching/binarySearch.cpp
@@ -3,17 +3,16 @@ vector<int> bianrySeach(vector<int> arr, int target){
     int s = 0;
     int e = arr.size() - 1;
     while(s <= e){
-        int mid = s + (e - s) / 2;
-
-        if(arr[mid] == target){
-            return {mid};
-        }
-        else if(arr[mid] > target){
-            e = mid - 1;
-        }
-        else if(target > arr[mid]){
+       int mid = s + (e - s)/2;
+       if(arr[mid] == target){
+            return { mid };
+       }
+       else if(arr[mid] < target){
             s = mid + 1;
-        }
+       }
+       else{
+            e = mid - 1;
+       }
     }
     return { -1 };
 }
--- 06_practice/Searching/peakElement.cpp
+++ 06_practice/Searching/peakElement.cpp
@@ -1,19 +1,6 @@
 #include<bits/stdc++.h>
 vector<int> peakELement(vector<int> arr){
-    int n = arr.size();
-    int s = 0;
-    int e = arr.size() - 1;
-    int ans = -1;
-    while(s <= e){
-        int mid = s + (e - s) / 2;
-        if(arr[mid] < arr[mid + 1]){
-            s = mid + 1;
-        }
-        else if(arr[mid] > arr[mid + 1]){
-            ans = mid;
-            e = mid - 1;
-        }
-     }
+   
     return { ans };
 }
 int main () {

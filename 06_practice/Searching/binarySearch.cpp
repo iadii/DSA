d1c2f7f13f1123a12fb40e08679b2f058dbb11b9
iadii <mishraaditya3456@gmail.com>
2026-07-18 20:46:15 +0530

    ..

--- 06_practice/Searching/binarySearch.cpp
+++ 06_practice/Searching/binarySearch.cpp
@@ -1,28 +1,27 @@
 #include<bits/stdc++.h>
-vector<int> bianrySeach(vector<int> arr, int target){
-    int s = 0;
-    int e = arr.size() - 1;
-    while(s <= e){
-       int mid = s + (e - s)/2;
-       if(arr[mid] == target){
-            return { mid };
-       }
-       else if(arr[mid] < target){
-            s = mid + 1;
-       }
-       else{
-            e = mid - 1;
-       }
-    }
-    return { -1 };
+vector<int> binarySearch(vector<int> arr, int target){
+   int s = 0;
+   int e = arr.size() - 1;
+   int ans = -1;
+   while(s <= e){
+      int mid = s + (e - s)/2;
+      if(arr[mid] == target){
+         ans = mid;
+         return {ans};
+      }
+      else if(arr[mid] > target){
+         e = mid - 1;
+      }
+      else {
+         s = mid + 1;
+      }
+   }
+   return {ans};
 }
 int main () {
-
-    vector<int> arr = {
-        10, 20, 30, 40, 50, 60, 70
-    };
-    int target = 30;
-    vector<int> val = bianrySeach(arr, target);
-    cout<<"Element is found at: "<<val[0]<<" and value is: "<<arr[val[0]];
+   vector<int> arr = {10, 20, 30, 40, 50, 60};
+   int target = 40;
+   vector<int> ans = binarySearch(arr, target);
+   cout<<"target elemnt is at index "<<ans[0] <<"and element is "<<arr[ans[0]];
    return 0;
 }

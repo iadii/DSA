405f5e741bfe53e3a96054f8fae69c4612f9023e
iadii <mishraaditya3456@gmail.com>
2026-09-01 12:41:56 +0530

    pract pivotted element

--- 06_practice/Searching/pivotPoint.cpp
+++ 06_practice/Searching/pivotPoint.cpp
@@ -4,6 +4,9 @@ vector<int> pivotElement(vector<int> arr){
    int e = arr.size() - 1;
    int ans = -1;
    while(s <= e){
+    if(s == e){
+        ans = s;
+    }
     int mid = s + (e - s)/2;
     if(arr[mid] < arr[mid - 1]){
         ans = mid -1 ;
@@ -14,14 +17,14 @@ vector<int> pivotElement(vector<int> arr){
     else if(arr[mid] > arr[s]){
         s = mid + 1;
     }
-    else if(arr[mid] < arr[s]){
+    else if(arr[mid] < arr[s]){ 
         e = mid - 1;
     }
    }
    return {ans};
 }
 int main () {
-   vector<int> arr = {10, 20, 30, 40, 50, 60};
+   vector<int> arr = {12, 14, 16, 2, 4, 6, 8, 10};
    vector<int> ans = pivotElement(arr);
    cout<<"pivotted elemnt is at index "<<ans[0] <<"and element is "<<arr[ans[0]];
    return 0;

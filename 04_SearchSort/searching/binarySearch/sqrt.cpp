f7509feac86e276332ea5f0fafc89c205870922b
iadii <mishraaditya3456@gmail.com>
2026-09-14 12:58:07 +0530

    sqrt of num

---
+++ 04_SearchSort/searching/binarySearch/sqrt.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+int sqr(int x){
+    int low = 0;
+    int high = x;
+    int ans = 0;
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if((mid * mid) == x){
+            return mid;
+        }
+        else if((mid * mid) >= x){
+            high = mid - 1;
+        }
+        else {
+            ans = mid;
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+int main () {
+   int x = 48;
+   int ans = sqr(x);
+   cout<<"square root of: "<<x<<" is "<<ans;
+   return 0;
+}

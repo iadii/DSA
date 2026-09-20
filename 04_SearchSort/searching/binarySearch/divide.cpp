334e393dbf085ed3ddc5c6d8fd2fdc756cb87e86
iadii <mishraaditya3456@gmail.com>
2026-09-20 10:51:14 +0530

    --no-ammend

---
+++ 04_SearchSort/searching/binarySearch/divide.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+int dvd(int x, int y){
+    int low = 0;
+    int h = x;
+    int ans = 0;
+    while(low <= h){
+        int mid = low + (h - low) /2;
+        if(y * mid == x){
+            return mid;
+        }
+        else if (y * mid <= x){
+            ans = mid;
+            low = mid + 1;
+        }
+        else{
+            h = mid - 1;
+        }
+    }
+    return ans;
+}
+int main () {
+   int x = 48;
+   int y = 5;
+   int ans = dvd(x, y);
+   cout<<"Division of "<<x<<" and "<<y<<" is: "<<ans;
+   return 0;
+}
